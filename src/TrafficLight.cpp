777135e15148f05857d57596713b77c71ae12c70
Mattia Haas <mattia.haas@traila.ch>
2024-03-07 22:20:30 +0100

    FP.2, implement traffic light phase cycling

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -41,9 +41,12 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
     return _currentPhase;
 }
 
+*/
+
 void TrafficLight::simulate()
 {
     // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+  threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
@@ -52,7 +55,39 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+  std::random_device os_seed;
+  const uint_least32_t seed = os_seed();
 
-*/
+  std::mt19937 generator(seed);
+  std::uniform_real_distribution<double> distribute(4.0,6.0);
+  
+  double random_wait;
+  
+  auto t1 = std::chrono::high_resolution_clock::now();
+  auto t2 = std::chrono::high_resolution_clock::now();
+  while (true){
+    
+    // Update the current time and compute the cycle time
+    t2 = std::chrono::high_resolution_clock::now();
+    double dt = (std::chrono::duration<double>(t2-t1)).count();
+    
+    // Compute and sleep for random wait time to wait for 4-6 s
+    random_wait = distribute(generator) - dt;
+    std::this_thread::sleep_for(std::chrono::duration<double>(random_wait));
+    
+    // Toggle from red to green or from green to red
+    if (_currentPhase==TrafficLightPhase::red){
+      _currentPhase = TrafficLightPhase::green;
+    } else if (_currentPhase==TrafficLightPhase::green) {
+      _currentPhase = TrafficLightPhase::red;
+    }
+    
+    // Send light phase to queue
+    _light_singals.send(std::move(_currentPhase));
+    
+    // Sleep for 1 millisecond
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    t1 = t2;
+  }
+}
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -56,6 +56,7 @@ private:
     std::condition_variable _condition;
     std::mutex _mutex;
     TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _light_singals;
 };
 
 #endif
